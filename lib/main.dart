79b9212a91d7dd3965aea73ab21cd3bf85004766
Bullets-jyr <jyr@bullets.co.kr>
2023-11-29 16:18:00 +0900

    Import data from Rest API

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:routine_app/collections/category.dart';
+import 'package:routine_app/collections/product.dart';
 import 'package:routine_app/collections/routine.dart';
 import 'package:routine_app/screens/create_routine.dart';
 import 'package:routine_app/screens/update_routine.dart';
+import 'package:routine_app/httpServer.dart';
+import 'package:logger/logger.dart';
+
+import 'config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationDocumentsDirectory();
   // FIXME 2 : Isar will no longer create the provided directory. Make sure it exists before opening an Isar Instance. ?
   final isar = await Isar.open(
-    [RoutineSchema, CategorySchema],
+    [RoutineSchema, CategorySchema, ProductSchema],
     directory: dir.path,
   );
   runApp(
@@ -63,6 +69,7 @@ class _MainPageState extends State<MainPage> {
   bool searching = false;
   String feedback = "";
   MaterialColor feedbackColor = Colors.blue;
+  HttpService httpService = HttpService();
 
   @override
   void initState() {
@@ -89,6 +96,12 @@ class _MainPageState extends State<MainPage> {
               );
             },
             icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              _apitoisar();
+            },
+            icon: const Icon(Icons.download),
           )
         ],
       ),
@@ -120,14 +133,68 @@ class _MainPageState extends State<MainPage> {
               ),
             ),
             FutureBuilder<List<Widget>>(
-                future: _buildWidgets(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(children: snapshot.data!);
+              future: _buildWidgets(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(children: snapshot.data!);
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+            FutureBuilder<List<Product>>(
+              future: generateProducts(),
+              builder: ((context, snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data!.isNotEmpty) {
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      physics: const ScrollPhysics(),
+                      children: List.generate(
+                        snapshot.data!.length,
+                        (index) {
+                          return Card(
+                            elevation: 4.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width * 0.4,
+                                    height: 90,
+                                    child: Image.network(
+                                      snapshot.data![index].image!,
+                                      fit: BoxFit.fitHeight,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: Text(
+                                      snapshot.data![index].title!,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    child: const Text("View"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
                   } else {
                     return const SizedBox();
                   }
-                }),
+                } else {
+                  return const SizedBox();
+                }
+              }),
+            ),
           ],
         ),
       ),
@@ -244,7 +311,8 @@ class _MainPageState extends State<MainPage> {
   searchRoutineByName(String searchName) async {
     searching = true;
     final routineCollection = widget.isar.routines;
-    final searchResults = await routineCollection.filter().titleContains(searchName).findAll();
+    final searchResults =
+        await routineCollection.filter().titleContains(searchName).findAll();
     setState(() {
       routines = searchResults;
     });
@@ -280,5 +348,32 @@ class _MainPageState extends State<MainPage> {
         feedbackColor = Colors.blue;
       }
     });
+  }
+
+  _apitoisar() async {
+    httpService.init(
+      BaseOptions(
+        baseUrl: baseUrl,
+        contentType: "application/json",
+      ),
+    );
+
+    final response = await httpService.request(
+        endpoint: "products?limit=6", method: Method.GET);
+
+    List<Map<String, dynamic>>? products = (response.data as List)
+        .map((item) => item as Map<String, dynamic>)
+        .toList();
+
+    await widget.isar.writeTxn(() async {
+      await widget.isar.products.clear();
+      await widget.isar.products.importJson(products);
+    });
+    //setState(() {});
+  }
+
+  Future<List<Product>> generateProducts() async {
+    List<Product> getProducts = await widget.isar.products.where().findAll();
+    return getProducts;
   }
 }
